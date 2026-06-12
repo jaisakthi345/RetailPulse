5a13141c422758334371225a0c3c1d4db6a2478d
Jaisakthi.R <jaisakthi3456@gmail.com>
2026-06-12 19:01:45 +0530

    Remove Python version info from app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-Python 3.14.5 (v3.14.5:5607950ef23, May 10 2026, 07:38:09) [Clang 21.0.0 (clang-2100.0.123.102)] on darwin
-Enter "help" below or click "Help" above for more information.
 import streamlit as st
 import pandas as pd
 import numpy as np
